9b15701df9e24a76ff6a715d551424f3aab6454a
Amanuel Getachew <amantwd@gmail.com>
2021-12-18 17:47:00 +0100

    add state machine interceptor

--- src/main/java/com/workmotion/ems/service/WorkFlowServiceImpl.java
+++ src/main/java/com/workmotion/ems/service/WorkFlowServiceImpl.java
@@ -3,7 +3,6 @@ package com.workmotion.ems.service;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.statemachine.StateMachine;
-import org.springframework.statemachine.config.StateMachineFactory;
 import org.springframework.statemachine.service.StateMachineService;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +10,7 @@ import com.workmotion.ems.dal.model.Employee;
 import com.workmotion.ems.util.EMSException;
 import com.workmotion.ems.util.ExceptionEnums;
 import com.workmotion.ems.workflow.WorkFlowEvents;
+import com.workmotion.ems.workflow.WorkFlowStateMachineInterceptor;
 import com.workmotion.ems.workflow.WorkFlowStates;
 
 import lombok.RequiredArgsConstructor;
@@ -22,12 +22,19 @@ import lombok.extern.slf4j.Slf4j;
 public class WorkFlowServiceImpl implements IWorkFlowService {
 
     private final StateMachineService<WorkFlowStates, WorkFlowEvents> stateMachineService;
+    private final WorkFlowStateMachineInterceptor stateMachineInterceptor;
 
     @Override
     public boolean executeTransition(Employee employee, WorkFlowStates state) throws EMSException {
         final String methodName = "executeTransition()";
         StateMachine<WorkFlowStates, WorkFlowEvents> sm = stateMachineService.acquireStateMachine(employee.getId().toString());
-        WorkFlowEvents event = WorkFlowEvents.get(employee.getStatus(), state.name());
+        
+        WorkFlowEvents event = null;
+        for(WorkFlowStates st : sm.getState().getIds()) {
+            event = WorkFlowEvents.get(st.name(), state.name());
+            if(event != null) break;
+        }
+        
         if(event == null) {
             log.error("{} Transition to state: {} not allowed.", methodName, state.name());
             throw ExceptionEnums.STATE_TRANSITION_EXCEPTION.get();
@@ -37,6 +44,7 @@ public class WorkFlowServiceImpl implements IWorkFlowService {
                 .setHeader("employeeId", employee.getId())
                 .build();
         
+        sm.getStateMachineAccessor().doWithAllRegions(sma -> sma.addStateMachineInterceptor(stateMachineInterceptor));
         return sm.sendEvent(message);
     }
 }
---
+++ src/main/java/com/workmotion/ems/workflow/WorkFlowStateMachineInterceptor.java
@@ -0,0 +1,60 @@
+package com.workmotion.ems.workflow;
+
+import java.util.Iterator;
+
+import org.springframework.messaging.Message;
+import org.springframework.statemachine.StateMachine;
+import org.springframework.statemachine.state.State;
+import org.springframework.statemachine.support.StateMachineInterceptorAdapter;
+import org.springframework.statemachine.transition.Transition;
+import org.springframework.stereotype.Component;
+
+import com.workmotion.ems.dal.model.Employee;
+import com.workmotion.ems.dal.repository.EmployeeRepository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Intercept and manage update operations
+ * 
+ */
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class WorkFlowStateMachineInterceptor extends StateMachineInterceptorAdapter<WorkFlowStates, WorkFlowEvents> {
+
+    private final EmployeeRepository employeeRepository;
+
+    @Override
+    public void postStateChange(State<WorkFlowStates, WorkFlowEvents> state, Message<WorkFlowEvents> message,
+            Transition<WorkFlowStates, WorkFlowEvents> transition,
+            StateMachine<WorkFlowStates, WorkFlowEvents> stateMachine,
+            StateMachine<WorkFlowStates, WorkFlowEvents> rootStateMachine) {
+
+        Employee employee = employeeRepository.findById(Integer.valueOf(rootStateMachine.getId())).orElse(null);
+
+        if (employee == null) {
+            log.error("Employee data not found, unable to update status.");
+            return;
+        }
+
+        StringBuilder builder = new StringBuilder();
+        Iterator<WorkFlowStates> statesIterator = rootStateMachine.getState().getIds().iterator();
+        while (statesIterator.hasNext()) {
+            builder.append(statesIterator.next().name());
+            if (statesIterator.hasNext()) {
+                builder.append("#");
+            }
+        }
+        employee.setStatus(builder.toString());
+        employeeRepository.save(employee);
+    }
+
+    @Override
+    public Exception stateMachineError(StateMachine<WorkFlowStates, WorkFlowEvents> stateMachine, Exception exception) {
+        log.error("{} StateMachine encountered error: [ message: {}]", "stateMachineError()", exception.getMessage());
+        return super.stateMachineError(stateMachine, exception);
+    }
+
+}
